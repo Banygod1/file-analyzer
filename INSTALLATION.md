c79851efa24c6b00861b2c18e6d885891855bf3b
Banygod1 <juniorbrignolle@gmail.com>
2026-03-03 21:35:39 -0500

    Add main files

---
+++ INSTALLATION.md
@@ -0,0 +1,34 @@
+## Installation
+
+### 1. Clone the project (or download it)
+
+```bash
+git clone https://github.com/Banygod1/file-analyzer.git
+cd File_Analyzer
+```
+
+### 2. Create and activate a virtual environment
+
+```bash
+python -m venv .venv
+.venv\Scripts\activate
+```
+
+On Linux/macOS, activation is:
+
+```bash
+source .venv/bin/activate
+```
+
+### 3. Install dependencies
+
+```bash
+pip install -r requirements.txt
+```
+
+### 4. Run the program
+
+```bash
+python main.py
+```
+
---
+++ main.py
@@ -0,0 +1,61 @@
+import magic
+from pathlib import Path
+
+#File path initialization
+file_path = Path.home()
+
+while True:
+
+    #Current path
+    print(f"Current directory: {file_path}")
+
+    #File path input
+    path_obj = input("Insert the path to the file: ")
+    file_path = Path(path_obj)
+        
+    while not file_path.exists():
+        print("This path does not exist. Please insert a valid file path.")
+        path_obj = input("Insert the path to the file: ")
+        file_path = Path(path_obj)
+
+    #Print all files in current directory
+    if file_path.is_dir():
+        for item in file_path.iterdir():
+            print(item.name)
+
+    #Get MIME type (file format)
+    mime_type = magic.from_file(file_path, mime=True)
+
+    #Get human-readable file description
+    description = magic.from_file(file_path)
+
+    #Print MIME type and description
+    print(f"The file {file_path} has MIME type: {mime_type}")
+    print(f"Description: {description}")
+
+    #Print generic file type
+    if mime_type.startswith("image/"):
+        print("This is an image file.")
+
+    elif mime_type.startswith("video/"):
+        print("This is a video file.")
+
+    elif mime_type.startswith("audio/"):
+        print("This is an audio file.")
+
+    elif mime_type.startswith("application/"):
+        print("This is an application file.")
+
+    elif mime_type.startswith("text/"):
+        print("This is a text file.")
+
+    #Continue analyze
+    continue_analyze = input("Do you want to analyze another file? (y/n): ")
+    if continue_analyze.lower() == "n":
+        break
+    elif continue_analyze.lower() == "y":
+        analyze_file = True
+    else:
+        print("Invalid input. Please enter 'y' or 'n'.")
+
+print("Thank you for using the file analyzer!")
